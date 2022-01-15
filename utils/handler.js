947feccb47774438476f09efe5b96914201688ef
NAoHR <najmim625@gmail.com>
2022-01-15 16:09:28 +0700

    initial commit: needed module

---
+++ utils/handler.js
@@ -0,0 +1,60 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+const parseIt = (data) => {
+    return data.split(" ").join("+")
+}
+const requestsData = async (link) =>{
+    try{
+        const data = await axios({
+            "url" : link,
+            "method" : "GET",
+            "timeout" : 2000
+        });
+        return await data.data;
+    }catch(Err){
+        console.log(Err)
+        return false;
+    }
+}
+const validateRequests = (status,type)=>{
+    if(!status){
+        return {
+            "Err" : true,
+            "status" : "Not found",
+            "desc" : `make sure you type ${type} correctly`
+        }
+    }else{
+        const $ = cheerio.load(status);
+        if($("div.alert-warning").text() == ""){
+            return true
+        }
+        return false
+    }
+}
+
+
+const lyricsHandler = (data) =>{
+    const $ = cheerio.load(data);
+    data = $("table.table-condensed > tbody").children()
+    let list = []
+    data.each((i,el)=>{
+        let seperateIt = $(el).children("td").children("a").text().split(" - ");
+        list.push({
+            "title" : seperateIt[0].split("\"").join(" "),
+            "artist" : seperateIt[1],
+            "link" : $(el).children("td").children("a").attr("href")
+        })
+    })
+    return {
+        "Err" : false,
+        "data" : list
+    }
+    
+}
+module.exports = {
+    parseIt,
+    requestsData,
+    validateRequests,
+    lyricsHandler
+}
