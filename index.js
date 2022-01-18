06e7cd226278f1de686a56e3b5a6da5d528e1e55
NAoHR <najmim625@gmail.com>
2022-01-18 13:08:36 +0700

    minor change: change res.send to res.json

--- index.js
+++ index.js
@@ -7,36 +7,36 @@ const app = express();
 const port = 3000;
 
 app.get("/",(req,res)=>{
-    res.send(jsonData)
+    res.json(jsonData)
 })
 
 app.get("/lyrics",(req,res)=>{
-    res.send(jsonData.providedRoutes[1])
+    res.json(jsonData.providedRoutes[1])
 })
 app.get("/lyrics/:songLink",async (req,res)=>{
     const newTitle = parseIt(req.params["songLink"],"lyrics")
     const data = await requestsData(`https://www.azlyrics.com/lyrics/${newTitle}`);
     if(!data){
-        res.send({
+        res.json({
             "Err" : true,
             "status" : "song's lyrics not found",
             "message" : "please type it corrrectly (either the artist or the song)"
         });
     }else{
         const lyricsData = lyricsHandler(data);
-        res.send(lyricsData);
+        res.json(lyricsData);
     }
 })
 
 app.get("/search",(req,res)=>{
-    res.send(jsonData.providedRoutes[0])
+    res.json(jsonData.providedRoutes[0])
 })
 app.get("/search/:title",async (req,res)=>{
     const newTitle = parseIt(req.params['title'],"songs");
     const data = await requestsData(`https://search.azlyrics.com/search.php?q=${newTitle}&w=songs&p=1`)
     const validated = validateRequests(data,"title");
     if(!validated){
-        res.send({
+        res.json({
             "Err" : true,
             "status" : "song not found",
             "message" : "please type it corrrectly"
@@ -44,7 +44,7 @@ app.get("/search/:title",async (req,res)=>{
     }
     else{
         const newData = songHandler(data,`${req.protocol}://${req.get("host")}`)
-        res.send(newData);
+        res.json(newData);
     }
 })
 
