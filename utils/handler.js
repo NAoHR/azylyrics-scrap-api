224117a868383afd30e6fdc837163b222a7b817b
NAoHR <najmim625@gmail.com>
2022-01-23 17:17:33 +0700

    needed tools for new feature

--- utils/handler.js
+++ utils/handler.js
@@ -1,6 +1,7 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 
+// credential
 const parseIt = (data,type) => {
     if(type == "lyrics"){
         return data.split("_").join("/")
@@ -12,45 +13,112 @@ const requestsData = async (link) =>{
         const data = await axios({
             "url" : link,
             "method" : "GET",
-            "timeout" : 2000 // you can adjust the amount of this time in ms
+            "timeout" : 5000 // you can adjust the amount of this time in ms
         });
         return await data.data;
     }catch(Err){
-        return false;
+        return -1;
     }
 }
-const validateRequests = (status,type)=>{
-    if(!status){
-        return false
-    }else{
-        const $ = cheerio.load(status);
-        if($("div.alert-warning").text() == ""){
-            return true
+const validateRequests = (dataRes)=>{
+    const $ = cheerio.load(dataRes);
+
+    if($("div.alert-warning").text() == ""){
+        return true
+    }
+    return false
+}
+const generateLink = (cred) => {
+    const willbeProccededData = [
+        {
+            "type" : "t",
+            "nameType" : "title",
+            "name" : "titleBased",
+            "link" : "https://search.azlyrics.com/search.php?q=",
+            "query" :"&w=songs&p=1"
+        },
+        {
+            "type" : "l",
+            "nameType" : "lyrics",
+            "name" : "lyricBased",
+            "link" : "https://search.azlyrics.com/search.php?q=",
+            "query" :"&w=lyrics&p=1"
         }
-        return false
+    ]
+    if(cred["q"] != undefined){
+        if(cred["q"] == "t" || cred["q"] == "l"){
+            return willbeProccededData.filter((item)=> item.type == cred["q"]);
+    
+        }else if(cred["q"] == "all"){
+            return willbeProccededData;
+        }
     }
+    return [willbeProccededData[0]]
+}
+const mainRequest = async (generatedLink,parsedUserInput,host) =>{
+    const waitAllReq = await Promise.all(generatedLink.map(async (item) => {
+        const req = await requestsData(`${item['link']}${parsedUserInput}${item['query']}`);
+        if(req == -1){
+            return {
+                "status" : false,
+                "ErrorLog" : "requests went failed",
+                "name" : item["name"],
+                "willBeProccededData" : [],
+            }
+        }else{
+            if(validateRequests(req,"song")){
+                if(item.type == "t"){
+                    return {
+                        "status" : true,
+                        "ErrorLog" : `no Error`,
+                        "name" : item["name"],
+                        "willBeProccededData" : songHandler(req,host,item["type"]),
+                    }
+                }
+                return {
+                    "status" : true,
+                    "ErrorLog" : `no Error`,
+                    "name" : item["name"],
+                    "willBeProccededData" : songHandler(req,host,item["type"]),
+                }
+            }else{
+                return {
+                    "status" : false,
+                    "ErrorLog" : `make sure you type the ${item['nameType']} correctly`,
+                    "name" : item["name"],
+                    "willBeProccededData" : [],
+                }
+            }
+        }
+    }))
+    return await waitAllReq;
 }
 
-
-const songHandler = (data,host) =>{
+const songHandler = (data,host,type) =>{
     const $ = cheerio.load(data);
     data = $("table.table-condensed > tbody").children()
     let list = []
     data.each((i,el)=>{
         if($(el).children("td").attr("class") != undefined){
             let seperateIt = $(el).children("td").children("a").text().split(" - ");
-            list.push({
-                "title" : seperateIt[0].split("\"").join(" "),
-                "artist" : seperateIt[1],
-                "link" : `${host}/lyrics/${$(el).children("td").children("a").attr("href").split("/lyrics/")[1].split("/").join("_")}`
-            })
-        }
-    })
-    return {
-        "Err" : false,
-        "data" : list
-    }
-    
+            if(type == "l"){
+                let artistName = seperateIt[1].split("\n")
+                list.push({
+                    "title" : seperateIt[0].split("\"").join(" "),
+                    "artist" : artistName[0],
+                    "on" : artistName.slice(1).join(" ").replace("                     ",""),
+                    "link" : `${host}/lyrics/${$(el).children("td").children("a").attr("href").split("/lyrics/")[1].split("/").join("_")}`
+                });
+            }else{
+                list.push({
+                    "title" : seperateIt[0].split("\"").join(" "),
+                    "artist" : seperateIt[1],
+                    "link" : `${host}/lyrics/${$(el).children("td").children("a").attr("href").split("/lyrics/")[1].split("/").join("_")}`
+                });
+            }
+        };
+    });
+    return list;
 }
 
 const lyricsHandler = (data) => {
@@ -76,10 +144,13 @@ const lyricsHandler = (data) => {
     }
     return tobeReturned
 }
+
 module.exports = {
     parseIt,
     requestsData,
     validateRequests,
     songHandler,
-    lyricsHandler
+    lyricsHandler,
+    mainRequest,
+    generateLink
 }
