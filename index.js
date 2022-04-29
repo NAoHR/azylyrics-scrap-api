9c248bccee14c5bcc8e47b68f50295cf9b70e86c
NAoHR <najmim625@gmail.com>
2022-04-29 17:18:06 +0700

    refactor : refactor my old code

--- index.js
+++ index.js
@@ -1,68 +1,19 @@
 const express = require("express");
-const jsonData = require("./utils/jsonFile/example.json");
-const url = require("url");
-
-
-const { requestsData,parseIt,lyricsHandler,mainRequest, generateLink} = require("./utils/handler.js");
-
+const lyricsController = require("./controller/lyricsController");
 const app = express();
+const bodyParser = require("body-parser");
 
 app.use(express.json());
+app.use(bodyParser.urlencoded({
+    extended : true
+}))
 
-app.get("/",(req,res)=>{
-    res.json(jsonData)
-})
+app.get("/", lyricsController.index)
+app.get("/lyrics",lyricsController.lyrics);
+app.get("/lyrics/:songLink", lyricsController.songsLyrics)
 
-app.get("/lyrics",(req,res)=>{
-    res.json(jsonData.providedRoutes[1])
-})
-app.get("/lyrics/:songLink",async (req,res)=>{
-    try{
-        const urlNew = url.parse(req.url,true);
-        const newTitle = parseIt(req.params["songLink"],"lyrics")
-        const data = await requestsData(`https://www.azlyrics.com/lyrics/${newTitle}`);
-        if(!data){
-            return res.json({
-                "Err" : true,
-                "status" : "song's lyrics not found",
-                "message" : "please type it corrrectly (either the artist or the song)"
-            });
-        }else{
-            const lyricsData = lyricsHandler(data);
-            return res.json(lyricsData);
-        }
-    }catch(e){
-        return res.status(500).json({
-            status : true,
-            ErrorLog : "internal error"
-        })
-    }
-})
-
-app.get("/search-guide",(req,res)=>{
-    res.json(jsonData.providedRoutes[0])
-})
-app.post("/search",async (req,res)=>{
-    try{
-        const {songname} = req.body
-        if(songname){
-            const urlNew = url.parse(req.url,true);
-            const requestedData = parseIt(songname,"song")
-            const linkList = await generateLink(urlNew.query)
-            const data = await mainRequest(linkList,requestedData,`${req.protocol}://${req.get("host")}`);
-            return res.status(200).json(data);
-        }
-        return res.status(400).json({
-            status : false,
-            ErrorLog : "songname not found on body"
-        })
-    }catch(e){
-        return res.status(500).json({
-            status : true,
-            ErrorLog : "internal error"
-        })
-    }
-})
+app.get("/search-guide",lyricsController.searchGuide);
+app.post("/search", lyricsController.search);
 
 app.listen(process.env.PORT || 3000, () => {
     console.log(`app running`)
